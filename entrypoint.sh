1d543a0b1ee6d19d34d23ac58373967071b00439
Renato "Lond" Cerqueira <renato@apptweak.com>
2023-04-11 18:08:28 +0200

    Update glider to v0.16.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 set -e
 
+BASE_DIR=${APP_DIR}
+if [[ -z "${BASE_DIR}" ]]; then
+    BASE_DIR="/app"
+fi
+
 if [[ -z "${PROXY_LIST_URL}" ]]; then
     echo "Using mounted proxy list"
-    touch /app/proxy-list.txt
+    touch $BASE_DIR/proxy-list.txt
     echo " ---> Done"
 else
     echo "Downloading proxy list from $PROXY_LIST_URL"
-    curl -s $PROXY_LIST_URL > /app/proxy-list.txt
+    curl -s $PROXY_LIST_URL > $BASE_DIR/proxy-list.txt
     echo " ---> Done"
 fi
 
 echo "Adding proy list to config file"
 # Remove blank lines
-sed -i '/^$/d' /app/proxy-list.txt
+sed -i '/^$/d' $BASE_DIR/proxy-list.txt
 # Prepend forward= in front of each line to match glider config syntax
-sed -i 's/^/forward=/' /app/proxy-list.txt
+sed -i 's/^/forward=/' $BASE_DIR/proxy-list.txt
 # Randomize and happen 
-cat /app/proxy-list.txt | shuf >> /app/glider.conf
+cat $BASE_DIR/proxy-list.txt | shuf >> $BASE_DIR/glider.conf
 echo " ---> Done"
 
 echo "Using config file"
-cat /app/glider.conf
+cat $BASE_DIR/glider.conf
 
 echo ""
 echo "Starting process"
