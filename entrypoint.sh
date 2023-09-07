71a74ab97cc62a0715abb60701ed0ab4896fdf6c
Thyago Lopes <thyago@apptweak.com>
2023-09-07 15:33:52 +0200

    [BACK-593] Include direct IP list download from Bright Data

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,26 @@ if [[ -z "${PROXY_LIST_URL}" ]]; then
     echo "Using mounted proxy list"
     touch $BASE_DIR/proxy-list.txt
     echo " ---> Done"
-else
+elif [[ -z "${BRIGHT_DATA_API_TOKEN}"]]; then
     echo "Downloading proxy list from $PROXY_LIST_URL"
     curl -s $PROXY_LIST_URL > $BASE_DIR/proxy-list.txt
     echo " ---> Done"
+else
+    echo "Downloading proxy list from $PROXY_LIST_URL"
+    curl -s -H "Authorization: Bearer $BRIGHT_DATA_API_TOKEN" "$PROXY_LIST_URL" > $BASE_DIR/proxy-list.txt
+    echo "Correctly formatting proxy list file"
+    # Builds the URL with the username, password and the endpoint of Bright Data
+    SUBSTITUTION_PATTERN="s/^.*/http:\/\/${BRIGHT_DATA_USERNAME}-ip-&:${BRIGHT_DATA_PASSWORD}@brd.superproxy.io:22225/"
+    sed -i $SUBSTITUTION_PATTERN $BASE_DIR/proxy-list.txt
+    echo " ---> Done"
 fi
 
-echo "Adding proy list to config file"
+echo "Adding proxy list to config file"
 # Remove blank lines
 sed -i '/^$/d' $BASE_DIR/proxy-list.txt
 # Prepend forward= in front of each line to match glider config syntax
 sed -i 's/^/forward=/' $BASE_DIR/proxy-list.txt
-# Randomize and happen 
+# Randomize and append
 cat $BASE_DIR/proxy-list.txt | shuf >> $BASE_DIR/glider.conf
 echo " ---> Done"
 
